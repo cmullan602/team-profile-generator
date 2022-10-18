96894bb042572aa261909d958f293a8272ceb41d
Connor Mullan <cmullan602@gmail.com>
2022-10-17 21:31:21 -0700

    added starter files.

---
+++ index.js
@@ -0,0 +1,19 @@
+const { fstat } = require("fs");
+const Game = require("./lib/Employee");
+
+// Initialize a new Game object
+const game = new Game();
+
+// Start playing
+game.play();
+creatTeamMember(){
+    const
+
+}
+
+
+function createNewHTML(fileName, date) {
+    HTMLFormControlsCollection.log('createHTML is running');
+    fs.writeFile(fileName, data (err) =>
+    err ? console.log(err))
+}
